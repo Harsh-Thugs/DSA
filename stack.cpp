39edeea130590944c300c6bb370be146ada65be7
Harsh-Thugs <harshchavan5490@gmail.com>
2024-07-31 22:45:07 +0530

    Stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -17,13 +17,13 @@ void pop(){
         cout<<"stack underflow"<<endl;
     }
     else{
-        cout<<"the po element is"<<stack[top]<<endl;
+        cout<<"the removed element is"<<stack[top]<<endl;
         top--;
     }
 }
 void display(){
     if(top>=0){
-        cout<<"stack elements are: ";
+        cout<<"stack has: ";
         for(int i=top;i>=0;i--){
             cout<<stack[i]<<" ";
             cout<<endl;
@@ -35,16 +35,16 @@ void display(){
 }
 int main(){
     int ch,val;
-    cout<<"1) push in stack"<<endl;
-    cout<<"2) pop from stack"<<endl;
-    cout<<"3) display"<<endl;
-    cout<<"4) exit"<<endl;
+    cout<<"1) Add in stack"<<endl;
+    cout<<"2) Remove from stack"<<endl;
+    cout<<"3) Display"<<endl;
+    cout<<"4) Exit"<<endl;
     do{
-        cout<<"enter choice:"<<endl;
+        cout<<"Enter number:"<<endl;
         cin>>ch;
         switch(ch){
             case 1: {
-                cout<<"enter the value:"<<endl;
+                cout<<"Enter the element:"<<endl;
                 cin>>val;
                 push(val);
                 break;
